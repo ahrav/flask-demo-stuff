9a799c627705bc76c33385f1e8ac24dcd8372051
Ahrav Dutta <ahravdutta02@gmail.com>
2019-08-07 04:14:58 -0700

    flask rest

---
+++ flask-restful/app.py
@@ -0,0 +1,15 @@
+import flask from Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Student(Resource):
+
+    def get(self, name):
+        return {'student': name}
+
+api.add_resource(Student, '/student/<string:name>')
+
+app.run()
--- flask_demo/app.py
+++ flask_demo/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "Hello World"
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
+
+
+@app.route("/store/<string:name>")
+def get_store(name):
+    pass
 
 
 app.run(port=5000)
