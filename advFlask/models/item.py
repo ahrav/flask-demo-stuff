64524a3163a73882b16a60050c36b7fa2f6fd697
Ahrav Dutta <ahravdutta02@gmail.com>
2019-08-13 01:31:35 -0700

    add complex type hinting

--- advFlask/models/item.py
+++ advFlask/models/item.py
@@ -1,7 +1,9 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from db import db
 
+
+itemJSON = Dict[str, Union[int, str, float]]
 
 class ItemModel(db.Model):
     __tablename__ = "items"
@@ -18,7 +20,7 @@ class ItemModel(db.Model):
         self.price = price
         self.store_id = store_id
 
-    def json(self) -> Dict:
+    def json(self) -> ItemJSON:
         return {
             "id": self.id,
             "name": self.name,
