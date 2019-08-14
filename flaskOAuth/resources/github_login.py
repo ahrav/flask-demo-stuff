e7256a48ced10dba5c4addffe4f1577e10c760f8
Ahrav Dutta <ahravdutta02@gmail.com>
2019-08-14 03:28:46 -0700

    add login to site with github oauth

--- flaskOAuth/resources/github_login.py
+++ flaskOAuth/resources/github_login.py
@@ -1,6 +1,9 @@
-from flask import g
+from flask import g, request
 from flask_restful import Resource
+from flask_jwt_extended import create_access_token, create_refresh_token
 from oa import github
+
+from models.user import UserModel
 
 
 class GithubLogin(Resource):
@@ -15,7 +18,28 @@ class GithubAuthorize(Resource):
     @classmethod
     def get(cls):
         res = github.authorized_response()
+
+        if res is None or res.get("access_token") is None:
+            error_response = {
+                "error": request.args["error"],
+                "error_description": request.args["error_description"],
+            }
+            return error_response
+
         g.access_token = res["access_token"]
         github_user = github.get("user")
         github_username = github_user.data["login"]
-        return github_username
+
+        user = UserModel.find_by_username(github_username)
+
+        if not user:
+            user = UserModel(username=github_username, password=None)
+            user.save_to_db()
+
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(user.id)
+
+        return (
+            {"access_token": access_token, "refresh_token": refresh_token},
+            200,
+        )
