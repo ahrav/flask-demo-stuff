0c22d774c9471c0856b0f7385a6cd0f34c8739c8
Ahrav Dutta <ahravdutta02@gmail.com>
2019-08-21 15:47:31 -0700

    continue refactor of code to docker

--- flaskDocker/flask/app.py
+++ flaskDocker/flask/app.py
@@ -1,18 +1,42 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+from marshmallow import ValidationError
 
+from blacklist import BLACKLIST
+from db import db
+from flask import Flask, jsonify
+from ma import ma
 
 app = Flask(__name__)
+load_dotenv(".env", verbose=True)
+app.config.from_object("default_config")  # load default config
+app.config.from_envvar(
+    "APPLICATION_SETTINGS"
+)  # override with config.py for PROD
+
 api = Api(app)
 
 
-class Student(Resource):
-    def get(self, name):
-        return {"student": name}
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
-api.add_resource(Student, "/api/v1/student/<string:name>")
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err):
+    return jsonify(err.messages), 400
+
+
+jwt = JWTManager(app)
+
+
+@jwt.token_in_blacklist_loader
+def check_token_in_blacklist(decrypted_token):
+    return decrypted_token["jti"] in BLACKLIST
 
 
 if __name__ == "main":
+    db.init_app(app)
+    ma.init_app(app)
     app.run(host="0.0.0.0")
