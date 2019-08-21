f3154d2d608c9472320e86051d6777fa7c884cf0
Ahrav Dutta <ahravdutta02@gmail.com>
2019-08-21 09:19:06 -0700

    expand on demo flask app

--- flaskDocker/flask/app.py
+++ flaskDocker/flask/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def index():
-    return "hello world"
+class Student(Resource):
+    def get(self, name):
+        return {"student": name}
+
+
+api.add_resource(Student, "/api/v1/student/<string:name>")
 
 
 if __name__ == "main":
